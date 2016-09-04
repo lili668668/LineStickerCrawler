fe0c0fc293811e4c03202354e2ce81b36aaf6ade
Tzu-Yin Hong <lili668668@gmail.com>
2016-09-04 10:01:56 +0800

    check the sticker group is exist

--- line-sticker-crawler-all.py
+++ line-sticker-crawler-all.py
@@ -1,4 +1,5 @@
 import requests as RS
+from bs4 import BeautifulSoup as BS
 import os
 import shutil
 from time import sleep
@@ -6,30 +7,41 @@ import random
 
 base1 = "https://sdl-stickershop.line.naver.jp/products/0/0/1/"
 base2 = "/android/stickers/"
-sticker_id_start = 3006
-sticker_id_end = 3010
+web_base1 = "https://store.line.me/stickershop/product/"
+web_base2 = "/zh-Hant"
+select = "span.mdCMN09Image"
+sticker_id_start = 3105
+sticker_id_end = 3200
 file_type = ".png"
 file_store = "/home/ballfish/VM/crawler_line_sticker/stickers/"
-start = 695383
-
-i = start
 
 for sticker_id_i in range(sticker_id_start, sticker_id_end) :
-    dire = file_store + str(sticker_id_i) + "/"
-    os.makedirs(dire, exist_ok=True)
-    base = base1 + str(sticker_id_i) + base2
-    href = base + str(i) + file_type
-    pic = RS.get(href, stream=True)
-    while pic.status_code == 200:
-        path = dire + str(i) + file_type
-        picture = open(path, "wb")
-        shutil.copyfileobj(pic.raw, picture)
-        print(str(i) + " loading...")
-        picture.close()
-
-        i = i + 1
-        href = base + str(i) + file_type
-        pic = RS.get(href, stream=True)
+    web = web_base1 + str(sticker_id_i) + web_base2
+    print("Now is " + web)
+    web = RS.get(web)
+    if web.status_code == 200:
+        soup = BS(web.text, "html.parser")
+        stickers = soup.select(select)
+        if len(stickers) != 0 :
+            dire = file_store + str(sticker_id_i) + "/"
+            os.makedirs(dire, exist_ok=True)
+            base = base1 + str(sticker_id_i) + base2
+            for sticker in stickers :
+                i = sticker["data-sticker-id"]
+                href = base + str(i) + file_type
+                pic = RS.get(href, stream=True)
+                if pic.status_code == 200:
+                    path = dire + str(i) + file_type
+                    picture = open(path, "wb")
+                    shutil.copyfileobj(pic.raw, picture)
+                    print(str(i) + " loading...")
+                    picture.close()
+                else :
+                    print("Sticker load fail")
+        else :
+            print(str(sticker_id_i) + " there is no sticker")
+    else :
+        print("The page may not found")
 
     wait = random.randint(1, 5)
     print("waiting " + str(wait) + " seconds...")
