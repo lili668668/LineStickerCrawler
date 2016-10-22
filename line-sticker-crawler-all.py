f08a80b6488e085af0dfcd54d623e250d8b5e1d1
Tzu-Yin Hong <lili668668@gmail.com>
2016-10-22 18:05:59 +0800

    Add main file

--- line-sticker-crawler-all.py
+++ line-sticker-crawler-all.py
@@ -6,30 +6,31 @@ from time import sleep
 import random
 import settings
 
-base1 = "https://sdl-stickershop.line.naver.jp/products/0/0/1/"
-base2 = "/android/stickers/"
 web_base1 = "https://store.line.me/stickershop/product/"
 web_base2 = "/zh-Hant"
 select = "span.mdCMN09Image"
-sticker_id_start = 3105
-sticker_id_end = 3200
+sticker_id_start = 2074
+sticker_id_end = 3000
 file_type = ".png"
 file_store = settings.file_store
 
 for sticker_id_i in range(sticker_id_start, sticker_id_end) :
     web = web_base1 + str(sticker_id_i) + web_base2
     print("Now is " + web)
-    web = RS.get(web)
+    try :
+        web = RS.get(web, timeout=1)
+    except RequestException:
+        print("The page may timeout")
+        continue
     if web.status_code == 200:
         soup = BS(web.text, "html.parser")
         stickers = soup.select(select)
-        if len(stickers) != 0 :
-            dire = file_store + str(sticker_id_i) + "/"
+        dire = file_store + str(sticker_id_i) + "/"
+        if len(stickers) != 0 and not os.path.exists(dire):
             os.makedirs(dire, exist_ok=True)
-            base = base1 + str(sticker_id_i) + base2
             for sticker in stickers :
                 i = sticker["data-sticker-id"]
-                href = base + str(i) + file_type
+                href = sticker["style"].split("url(")[-1].split(");")[0]
                 pic = RS.get(href, stream=True)
                 if pic.status_code == 200:
                     path = dire + str(i) + file_type
@@ -44,7 +45,7 @@ for sticker_id_i in range(sticker_id_start, sticker_id_end) :
     else :
         print("The page may not found")
 
-    wait = random.randint(1, 5)
+    wait = random.randint(1, 10)
     print("waiting " + str(wait) + " seconds...")
     sleep(wait)
 
